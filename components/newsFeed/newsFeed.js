543136906479ed4b486b2d6ce560764315cebc38
Blac-Panda <oyefesotunmise@gmail.com>
2020-04-03 16:06:43 +0100

    file structures

---
+++ components/newsFeed/newsFeed.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default class NewsFeed {
+  return (
+    
+  );
+}
+
+export default NewsFeed;
---
+++ pages/homepage/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default class HomePage {
+  return (
+    
+  );
+}
+
+export default HomePage;
