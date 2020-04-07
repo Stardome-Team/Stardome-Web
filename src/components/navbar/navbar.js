cbdf97f5355a51e4c39a833be4592f6b7474e269
Blac-Panda <oyefesotunmise@gmail.com>
2020-04-07 22:30:04 +0100

    started navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Logo from "../../assets/stardome_logo.png";
+import Facebook from "../../assets/facebook.png";
+import Instagram from "../../assets/instagram.png";
+import Twitter from "../../assets/twitter.png";
+import Youtube from "../../assets/youtube.png";
 import './navbar.scss';
 
 const NavBar = () => (
-    <h1>NavBar</h1>
+    <nav className="navbar">
+      <div className="left-nav">
+        <div><img src={Logo} /></div>
+        <div>Home</div>
+        <div>Tournaments</div>
+        <div>Match Schedule</div>
+        <div>About</div>
+      </div>
+      <div className="right-nav">
+        <div>Sign In / Sign Up</div>
+        <div><img src={Instagram} /></div>
+        <div><img src={Twitter} /></div>
+        <div><img src={Youtube} /></div>
+        <div><img src={Facebook} /></div>
+      </div>
+    </nav>
   )
 
 export default NavBar;
