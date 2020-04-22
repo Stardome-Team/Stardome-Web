92ef8824c9fd1648e9f87302aae3ae782587fb61
Blac-Panda <oyefesotunmise@gmail.com>
2020-04-22 23:38:08 +0100

    working on responsiveness

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,7 +7,8 @@ import Youtube from "../../assets/img/youtube.png";
 import './navbar.scss';
 
 const NavBar = () => (
-    <div class="w3-container w3-bar w3-black">
+  <div>
+    <div class="w3-container w3-hide-medium w3-hide-small w3-bar w3-black">
     <div className="w3-bar-item"><div className="logo"><img className="logo-image" src={Logo} /></div></div>
     <div ><a href="#" class="w3-bar-item" id="nav-links">HOME</a></div>
     <div ><a href="#" class="w3-bar-item" id="nav-links">TOURNAMENTS</a></div>
@@ -20,6 +21,25 @@ const NavBar = () => (
     <div><a href="#" class="w3-bar-item w3-right"><img src={Youtube} /></a></div>
     <div><a href="#" class="w3-bar-item w3-right"><img src={Facebook} /></a></div>
     </div>
+    <div class="w3-container w3-hide-large w3-hide-small w3-bar w3-black">
+    <div className="w3-bar-item"><div className="logo"><img className="logo-image" src={Logo} /></div></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">HOME</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">TOURNAMENTS</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">MATCH SCHEDULES</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">NEWS</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">ABOUT</a></div>
+    <div><a href="#" class="w3-bar-item w3-button w3-right" id="nav-button">SIGN IN</a></div>
+    </div>
+    <div class="w3-container w3-sidebar w3-hide-large w3-hide-medium w3-bar-block w3-collapse w3-black" style={{right:0}}>
+    <div className="w3-bar-item"><div className="logo"><img className="logo-image" src={Logo} /></div></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">HOME</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">TOURNAMENTS</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">MATCH SCHEDULES</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">NEWS</a></div>
+    <div ><a href="#" class="w3-bar-item" id="nav-links">ABOUT</a></div>
+    <div><a href="#" class="w3-bar-item w3-button w3-right" id="nav-button">SIGN IN</a></div>
+    </div>
+  </div>
   )
 
 export default NavBar;
