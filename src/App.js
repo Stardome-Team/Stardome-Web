0fd67fa1c790c04d8bfcffb37a08ac85373054b6
Yisau Abdussamad <sammieyisau@gmail.com>
2020-04-23 21:51:23 +0100

    Implemented React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Component from 'react-dom'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homePage'
 
-const App = () => ( 
-      <div>
-        <HomePage/>
-      </div>
-)
+// const App = () => ( 
+//       <div>
+//         <HomePage/>
+//       </div>
+// )
+
+function App() {
+  return(
+    <Router>
+      <Switch>
+        <Route path = "/" exact component = {HomePage}></Route>
+        {/* <Route path = "" component = {}></Route> */}
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
