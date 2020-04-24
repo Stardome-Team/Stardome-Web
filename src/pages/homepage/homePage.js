c7f006534b845888f37dbf427407b633cf81e6af
Okafor Irene <okaforirenen@gmail.com>
2020-04-24 15:19:52 +0100

    removed my footer

--- src/pages/homepage/homePage.js
+++ src/pages/homepage/homePage.js
@@ -3,14 +3,12 @@ import './homePage.scss';
 import NavBar from '../../components/navbar/navbar'
 import TwitterFeed from '../../components/twitterFeed/twitterFeed'
 import HomePageCarousel from '../../components/homePageCarousel/homepagecarousel'
-import Footer from '../../components/footer/footer'
 
 const HomePage = () => (
   <div className="homepage">
     <NavBar/>
     <HomePageCarousel/>
     <TwitterFeed/>
-    <Footer/>
   </div>
   );
 
