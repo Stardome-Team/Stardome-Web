d157b7a853528ffd4085a5d268e6cfb7014356da
Blac-Panda <oyefesotunmise@gmail.com>
2020-05-02 11:12:00 +0100

    changed json format

---
+++ src/components/eventSchedule/schedule.data.js
@@ -0,0 +1,56 @@
+const SCHEDULE = [
+  {
+    event: [
+      {
+        name: "Call of Duty Lagos Regionals 2020",
+        id: 158,
+        slots: 64,
+        date: "2020-04-29T10:00:00.000Z",
+        description: "Regional finals for Lagos State Call of Duty Championship, covers all zone 2 area. Yaba, Akoka, all these an okoto yarns.",
+        week: 1
+      },
+      {
+        name: "Call of Duty Lagos Regionals 2020",
+        id: 158,
+        slots: 64,
+        date: "2020-04-29T10:00:00.000Z",
+        description: "Regional finals for Lagos State Call of Duty Championship, covers all zone 2 area. Yaba, Akoka, all these an okoto yarns.",
+        week: 1
+      },
+      {
+        name: "Call of Duty Lagos Regionals 2020",
+        id: 158,
+        slots: 64,
+        date: "2020-04-29T10:00:00.000Z",
+        description: "Regional finals for Lagos State Call of Duty Championship, covers all zone 2 area. Yaba, Akoka, all these an okoto yarns.",
+        week: 1
+      },
+      {
+        name: "Call of Duty Lagos Regionals 2020",
+        id: 158,
+        slots: 64,
+        date: "2020-04-29T10:00:00.000Z",
+        description: "Regional finals for Lagos State Call of Duty Championship, covers all zone 2 area. Yaba, Akoka, all these an okoto yarns.",
+        week: 1
+      },
+      {
+        name: "Call of Duty Lagos Regionals 2020",
+        id: 158,
+        slots: 64,
+        date: "2020-04-29T10:00:00.000Z",
+        description: "Regional finals for Lagos State Call of Duty Championship, covers all zone 2 area. Yaba, Akoka, all these an okoto yarns.",
+        week: 1
+      },
+      {
+        name: "Call of Duty Lagos Regionals 2020",
+        id: 158,
+        slots: 64,
+        date: "2020-04-29T10:00:00.000Z",
+        description: "Regional finals for Lagos State Call of Duty Championship, covers all zone 2 area. Yaba, Akoka, all these an okoto yarns.",
+        week: 1
+      }
+    ]
+  }
+]
+
+export default SCHEDULE;
