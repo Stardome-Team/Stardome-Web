f2dad5bffb4753e66f011322be978f68c3ad8d0e
Okafor Irene <okaforirenen@gmail.com>
2020-05-02 23:26:02 +0100

    done stuff

--- src/components/UpComing/TournamentList/TournamentItem/TournamentItem.js
+++ src/components/UpComing/TournamentList/TournamentItem/TournamentItem.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+import "./TournamentItem.scss";
 
-export default function TournamentItem() {
+export default function TournamentItem({name,slots}) {
     return (
-        <div>
-            
+        <div className="Event">
+
+          
+
+        <div className="Details">
+
+        <p className="Title">{name}</p>
+        <p>Details of events</p>
+        <p>{slots} slots</p>
+                
+        </div>
+
+        <NavLink to="/">
+            Participate
+        </NavLink> 
+        
+           
         </div>
     )
 }
