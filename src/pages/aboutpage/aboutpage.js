6c89601df0df6136efee5ef7fbc175699336bfb9
Blac-Panda <oyefesotunmise@gmail.com>
2020-05-26 00:29:15 +0100

    about page design change, still need styling work

--- src/pages/aboutpage/aboutpage.js
+++ src/pages/aboutpage/aboutpage.js
@@ -33,16 +33,7 @@ class aboutPage extends React.Component {
     <div className="aboutpage">
     <div className="bg-image"></div>
         <NavBar/>
-        <div className = "aboutMessage">
-            <h2><strong>About Stardome</strong></h2>
-            <p>Consequat sit Lorem exercitation anim Lorem proident tempor cupidatat elit consectetur sint proident enim adipisicing. Excepteur id ipsum anim pariatur non ad. Exercitation sunt do aliqua ipsum magna consequat occaecat sunt cillum officia nostrud. Id voluptate ut minim quis. Et ea et quis nostrud anim velit occaecat laborum laborum eu nisi ad.
-
-            Ipsum eiusmod culpa sunt aliquip. In nulla ut aute labore aute. Magna do aliquip ullamco aute magna nisi veniam ut excepteur et incididunt aute incididunt laboris. Fugiat elit adipisicing cillum irure voluptate duis dolore est esse pariatur occaecat fugiat ad. Dolore sint fugiat nulla esse dolor ullamco in minim occaecat anim aliqua id.
-
-            Elit voluptate aute et fugiat nisi culpa ex pariatur ea do duis commodo. Id eiusmod in consequat minim do esse non ut nostrud non laborum labore. Ex occaecat commodo in veniam. Esse sint cupidatat adipisicing laboris voluptate in id. Laborum quis magna aliquip dolor proident velit deserunt commodo.
-
-            Ut ut officia commodo adipisicing aliqua do duis fugiat velit. Irure non non ea voluptate quis. Duis enim pariatur proident duis fugiat velit sit fugiat non. Proident exercitation nostrud officia quis. Quis culpa voluptate non dolore quis mollit cupidatat elit aliquip.Ea id adipisicing sunt incididunt pariatur. Ipsum et eiusmod nisi consequat duis amet. Labore laboris amet minim ullamco irure quis velit. Consequat nostrud qui mollit aute tempor fugiat esse commodo Lorem eiusmod ad.Ullamco sit esse amet ut tempor Lorem. Deserunt labore quis ut tempor nulla occaecat do ut aliqua qui dolor est ea minim. Fugiat dolor reprehenderit sit commodo qui laborum amet magna sit elit excepteur deserunt. Elit eu aute esse dolore et cillum eu proident. Deserunt mollit excepteur minim ex officia. Sit do labore do magna ex. Ut do qui ullamco laborum incididunt aliqua laborum non occaecat dolor est cupidatat proident.</p>
-        </div>
+        
 
         <div className="aboutGrid">
             <div className = "gridElement">
@@ -62,6 +53,17 @@ class aboutPage extends React.Component {
                 
             </div>
         
+        </div>
+
+        <div className = "aboutMessage">
+            {/* <h2><strong>About Stardome</strong></h2> */}
+            <p>Consequat sit Lorem exercitation anim Lorem proident tempor cupidatat elit consectetur sint proident enim adipisicing. Excepteur id ipsum anim pariatur non ad. Exercitation sunt do aliqua ipsum magna consequat occaecat sunt cillum officia nostrud. Id voluptate ut minim quis. Et ea et quis nostrud anim velit occaecat laborum laborum eu nisi ad.
+
+            Ipsum eiusmod culpa sunt aliquip. In nulla ut aute labore aute. Magna do aliquip ullamco aute magna nisi veniam ut excepteur et incididunt aute incididunt laboris. Fugiat elit adipisicing cillum irure voluptate duis dolore est esse pariatur occaecat fugiat ad. Dolore sint fugiat nulla esse dolor ullamco in minim occaecat anim aliqua id.
+
+            Elit voluptate aute et fugiat nisi culpa ex pariatur ea do duis commodo. Id eiusmod in consequat minim do esse non ut nostrud non laborum labore. Ex occaecat commodo in veniam. Esse sint cupidatat adipisicing laboris voluptate in id. Laborum quis magna aliquip dolor proident velit deserunt commodo.
+
+            Ut ut officia commodo adipisicing aliqua do duis fugiat velit. Irure non non ea voluptate quis. Duis enim pariatur proident duis fugiat velit sit fugiat non. Proident exercitation nostrud officia quis. Quis culpa voluptate non dolore quis mollit cupidatat elit aliquip.Ea id adipisicing sunt incididunt pariatur. Ipsum et eiusmod nisi consequat duis amet. Labore laboris amet minim ullamco irure quis velit. Consequat nostrud qui mollit aute tempor fugiat esse commodo Lorem eiusmod ad.Ullamco sit esse amet ut tempor Lorem. Deserunt labore quis ut tempor nulla occaecat do ut aliqua qui dolor est ea minim. Fugiat dolor reprehenderit sit commodo qui laborum amet magna sit elit excepteur deserunt. Elit eu aute esse dolore et cillum eu proident. Deserunt mollit excepteur minim ex officia. Sit do labore do magna ex. Ut do qui ullamco laborum incididunt aliqua laborum non occaecat dolor est cupidatat proident.</p>
         </div>
 
         <div className="about-grid">
