7abdf8ee7b17ac1317f7a9aac75ab27f7815f0fa
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-19 23:10:48 +0100

    Added basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import {AuthContext} from './context/auth'
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>,
   document.getElementById('root')
